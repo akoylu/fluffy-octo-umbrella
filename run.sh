f24b11113842d65215e99c548aff41f5a5f742c9
Ahmet Köylü <akoylu@gmail.com>
2022-01-07 23:57:58 +0300

    changes

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-echo -n "" > modules.xml
-echo "<a>" >> modules.xml
+echo -n "" > ./target/libs/modules.xml
+echo "<a>" >> ./target/libs/modules.xml
 
 for entry in `(cd ./target/libs && ls *.jar)`
 do
-  echo "  <b>$entry</b>" >> modules.xml
+  echo "  <b>$entry</b>" >> ./target/libs/modules.xml
 done
 
-echo "</a>" >> modules.xml
+echo "</a>" >> ./target/libs/modules.xml
